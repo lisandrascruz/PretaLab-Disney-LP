03802dadd7763866a71ec43652b8873e13d3893c
Renata Pereira <84108009+repereira@users.noreply.github.com>
2023-02-16 15:42:55 -0300

    Create exemplosAula15fevereiro.js

---
+++ semana02/aula05/Turma 1/exemplosAula15fevereiro.js
@@ -0,0 +1,34 @@
+const primeiroNome ="Renata",
+    ultimoNome = "Pereira",
+    anoDenascimento = "1989",
+    corPreferida = "laranja",
+    hobby = "comer",
+    umSentimento = "alegria";
+
+//console.log("Meu nome é " + primeiroNome + " "+ ultimoNome + "..." + umSentimento )
+//console.log(`Meu nome é ${primeiroNome} ${ultimoNome} ... ${umSentimento}`)
+
+
+//----------------------------------- Manipulando Strings
+
+let meuNome = "Renata Nunes Pereira";
+
+//console.log(meuNome.length)
+
+//console.log(meuNome.split(" "))
+
+//console.log(meuNome.replace("Renata","Roberta"))
+
+//----------Estrutura condicional:
+
+let hora = 13;
+
+if(hora < 12){
+   let a = "Bom dia!"
+}  else if(hora<18){
+   let  a = "Boa tarde!"
+} else {
+    let a = "Boa noite!"
+}
+
+console.log(a)
